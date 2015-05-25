82e65ce5c19ac84ad34ee3611005ddaddb714f71
dr0ptp4kt <abaso@wikimedia.org>
2015-05-25 08:48:11 -0700

    POC

--- Wiki Radio/ViewController.swift
+++ Wiki Radio/ViewController.swift
@@ -19,12 +19,15 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, NSURLSessionDeleg
     var audioSession: AVAudioSession?
     var interruptedOnPlayback = false
     var playing = false
-    var paused = false;
+    var paused = false
     var urlSession: NSURLSession!
     var currentSong: NSData?
     var nextSong: NSData?
     var retrievedFirstSong = false
     var playedFirstSong = false
+    var categoriesArray: NSArray?
+    var tracks: [String] = []
+    var audioURLs : [String] = []
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -81,6 +84,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, NSURLSessionDeleg
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        retrieveCategories()
     }
     
     func playNext() {
@@ -176,13 +180,25 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, NSURLSessionDeleg
             return
         }
         
-        let url = NSURL(string: "https://upload.wikimedia.org/wikipedia/commons/d/d4/Zaip.wav")
-        let task = self.urlSession.dataTaskWithURL(url!, completionHandler: {[weak self] (data: NSData!,
-            response: NSURLResponse!, error: NSError!) in
+        // TODO ensure that files have indeed been populated
+        // prior to trying to access them
+        
+        var url : NSURL?
+        if (self.audioURLs.count == 0) {
+            self.player!.pause()
+            self.paused = true
+            self.notPlaying()
+            return
+        }
+        var u = self.audioURLs[0]
+        println(u)
+        self.audioURLs.removeAtIndex(0)
+        url = NSURL(string: u)
+        let task = self.urlSession.dataTaskWithURL(url!, completionHandler: {[weak self] (data: NSData!, response: NSURLResponse!, error: NSError!) in
             /* We got our data here */
             self!.nextSong = data
             println("Done")
-            self!.urlSession.finishTasksAndInvalidate() })
+            /* self!.urlSession.finishTasksAndInvalidate() */ })
         println("about to resume")
         task.resume()
         println("resumed")
@@ -210,6 +226,180 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, NSURLSessionDeleg
         self.updatePlayingButton()
         self.playNext()
     }
+    
+    func retrieveCategories() {
+        // https://commons.wikimedia.org/w/api.php?action=opensearch&search=Category:Audio%20files&limit=100
+        
+        let url = NSURL(string: "https://commons.wikimedia.org/w/api.php?action=opensearch&search=Category:Audio%20files&limit=100")
+        let task = self.urlSession.dataTaskWithURL(url!, completionHandler: {[weak self] (data: NSData!, response: NSURLResponse!, error: NSError!) in
+            var jsonError: NSError?
+            let jsonObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &jsonError)
+            if jsonError == nil {
+                let array = jsonObject as! NSArray
+                if array.count > 1 {
+                    self!.categoriesArray = array[1] as! NSArray
+                    self!.retrieveFilePages()
+                }
+                
+            }
+            /* self!.urlSession.finishTasksAndInvalidate() */ })
+        println("about to resume")
+        task.resume()
+    }
+    
+    func retrieveFilePages() {
+        let divisor = 25
+        // let divisor = 1
+        let binSize = categoriesArray!.count / divisor
+        let randomOffset = Int(arc4random_uniform(UInt32(binSize)))
+        // let randomOffset = 0
+        println(randomOffset)
+        var categories : [String] = []
+        for i in 0...divisor-1 {
+            let randomCategoryPage = self.categoriesArray![i*binSize+randomOffset] as! String
+            println(randomCategoryPage)
+            let encodedRandomCategoryPage = randomCategoryPage.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet())!
+            
+            // https://commons.wikimedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Audio%20files&cmtype=file&cmlimit=500
+            let u = "https://commons.wikimedia.org/w/api.php?action=query&list=categorymembers&cmtitle=\(encodedRandomCategoryPage)&cmtype=file&cmlimit=500&format=json"
+            // println(u)
+            let url = NSURL(string: u)
+            retrieveFilePage(url!)
+            // println("still going")
+        }
+    }
+    
+    // FILE PAGE VERSION 2!
+    /*
+    func retrieveFilePages2() {
+        let divisor = 5
+        // let divisor = 1
+        let binSize = categoriesArray!.count / divisor
+        let randomOffset = Int(arc4random_uniform(UInt32(binSize)))
+        // let randomOffset = 0
+        println(randomOffset)
+        var categories : [String] = []
+        for i in 0...divisor-1 {
+            let randomCategoryPage = self.categoriesArray![i*binSize+randomOffset] as! String
+            println(randomCategoryPage)
+            let encodedRandomCategoryPage = randomCategoryPage.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet())!
+            
+            // https://commons.wikimedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Audio%20files&cmtype=file&cmlimit=500
+            let u = "https://commons.wikimedia.org/w/api.php?action=query&list=categorymembers&cmtitle=\(encodedRandomCategoryPage)&cmtype=file&cmlimit=50&format=json"
+            // println(u)
+            let url = NSURL(string: u)
+            retrieveFilePage(url!)
+            // println("still going")
+        }
+    }
+*/
+    
+    
+    
+    func retrieveFilePage(url: NSURL) {
+        let task = self.urlSession.dataTaskWithURL(url, completionHandler: {[weak self] (json: NSData!, response: NSURLResponse!, error: NSError!) in
+            
+            // println(NSString(data: json, encoding: NSUTF8StringEncoding))
+            var jsonError: NSError?
+            let jsonObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(json, options: NSJSONReadingOptions.AllowFragments, error: &jsonError)
+            if jsonError == nil {
+                if jsonObject is NSDictionary {
+                    let deserializedDictionary = jsonObject as! NSDictionary
+                    let query = deserializedDictionary["query"] as! NSDictionary
+                    if let categorymembers = query["categorymembers"] {
+                        let categorymembersArray = categorymembers as! NSArray
+                        //println(categorymembersArray)
+                        var count = 0
+                        for member in categorymembersArray {
+                            /*
+                            count++
+                            if (count > 40) {
+                                break;
+                            }
+                            if (count % 2 == 1) {
+                                continue
+                            }
+                            */
+                            let memberDict = member as! NSDictionary
+                            let fileTitle: String = memberDict["title"] as! String
+                            // println("file is \(fileTitle)")
+
+                            if fileTitle.hasSuffix(".wav") {
+                                if fileTitle.hasSuffix("Abhogi.wav") {
+                                    continue
+                                }
+                                let encodedFileTitle = fileTitle.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet())!
+                                self!.tracks.append(encodedFileTitle)
+                                self!.enqueueAudioURLs(encodedFileTitle)
+                            }
+                        }
+                    }
+                }
+            }
+            /* self!.urlSession.finishTasksAndInvalidate() */ })
+        task.resume()
+    }
+    
+    func enqueueAudioURLs(title: String) {
+        println("enqueueing")
+        
+        
+        let pipes: String = "canonicaltitle|size|mediatype|metadata|commonmetadata|extmetadata|bitdepth|derivatives".stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet())!
+        let u = "https://commons.wikimedia.org/w/api.php?action=query&titles=\(title)&prop=videoinfo&viprop=\(pipes)&format=json"
+        let url = NSURL(string: u)
+        
+        // let audioURL: NSURL? = NSURL(string: u)
+        /*
+        if (audioURL == nil) {
+        println("it's nil")
+        }
+        */
+        // println(u)
+        let task = self.urlSession.dataTaskWithURL(url!, completionHandler: {[weak self] (data: NSData!, response: NSURLResponse!, error: NSError!) in
+            var jsonError: NSError?
+            let jsonObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &jsonError)
+            if jsonError == nil {
+                if jsonObject is NSDictionary {
+                    let deserializedDictionary = jsonObject as! NSDictionary
+                    //println(deserializedDictionary)
+                    
+                    
+                    let query = deserializedDictionary["query"] as! NSDictionary
+                    let pages = query["pages"] as! NSDictionary
+                    let key = pages.allKeys[0] as! NSString
+                    let theObject = pages[key] as! NSDictionary
+                    let videoInfo = theObject["videoinfo"] as! NSArray
+                    let video = videoInfo[0] as! NSDictionary
+                    let derivatives = video["derivatives"] as! NSArray
+                    let record = derivatives[0] as! NSDictionary
+                    let src = record["src"] as! String
+                    println(src)
+                    self!.audioURLs.append(src)
+                    
+                    
+                    
+                    /*
+                    println(query)
+                    if let categorymembers = query["pages"] {
+                    let categorymembersArray = categorymembers as! NSArray
+                    //println(categorymembersArray)
+                    for member in categorymembersArray {
+                    let memberDict = member as! NSDictionary
+                    let fileTitle: String = memberDict["title"] as! String
+                    if fileTitle.hasSuffix(".wav") {
+                    let encodedFileTitle = fileTitle.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet())!
+                    self!.tracks.append(encodedFileTitle)
+                    */
+                }
+            }
+            /* self!.urlSession.finishTasksAndInvalidate() */ })
+        println("about to resume")
+        task.resume()
+        
+
+    }
+    
+    
     // defined for runtime safety
     func dummyPlayback() {
     }
